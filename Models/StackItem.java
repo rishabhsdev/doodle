3be46ffef28a79fccb90e0cd6e867a937e814af9
Rishabh Singh <56164824+rishabhsdev@users.noreply.github.com>
2024-02-11 19:46:21 +0530

    Create StackItem.java

---
+++ Models/StackItem.java
@@ -0,0 +1,22 @@
+package com.rishabhsingh.dev.search.Models;
+
+public class StackItem {
+    private String body;
+    private String title;
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
