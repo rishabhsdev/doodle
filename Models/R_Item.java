ded5de21d00b73bc409e24df32cccebc03489bf5
Rishabh Singh <56164824+rishabhsdev@users.noreply.github.com>
2024-02-11 19:47:43 +0530

    Create R_Item.java

---
+++ Models/R_Item.java
@@ -0,0 +1,32 @@
+package com.rishabhsingh.dev.search.Models;
+
+public class R_Item {
+    private String body;
+    private String title;
+    private String url;
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
